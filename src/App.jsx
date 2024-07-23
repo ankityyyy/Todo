c4af9ceb09fe58f4456882fda94056abb460db37
ankit dubey <ankitdubey9431969639@gmail.com>
2024-07-23 13:49:37 +0530

    add useReducer in store

--- src/App.jsx
+++ src/App.jsx
@@ -1,36 +1,19 @@
-import { useState } from "react";
 import "bootstrap/dist/css/bootstrap.min.css";
 import "./App.css";
 import TodoName from "./components/TodoName";
 import InputBox from "./components/InputBox";
-import { v4 as uuidv4 } from "uuid";
 import TodoList from "./components/TodoList";
-import { TodoContextProvider } from "./store/todo-item-store";
+import TodoContextProvide from "./store/todo-item-store";
 
 function App() {
-  const [todo, setTodo] = useState([
-    { task: "buy food", date: " 10 / 10 / 2003", id: uuidv4() },
-    { task: "food", date: "10 / 10 / 2003", id: uuidv4() },
-  ]);
-
-  const newTodo = (newTask, NewDate) => {
-    setTodo([...todo, { task: newTask, date: NewDate, id: uuidv4() }]);
-  };
-
-  const handleDelete = (id) => {
-    setTodo(todo.filter((todo) => todo.id !== id));
-  };
-
   return (
-    <TodoContextProvider.Provider
-      value={{ work: todo, deleteTodo: handleDelete, newItem: newTodo }}
-    >
+    <TodoContextProvide>
       <center>
         <TodoName />
         <InputBox />
         <TodoList />
       </center>
-    </TodoContextProvider.Provider>
+    </TodoContextProvide>
   );
 }
 
--- src/store/todo-item-store.jsx
+++ src/store/todo-item-store.jsx
@@ -1,5 +1,62 @@
 import { createContext } from "react";
+import { useReducer } from "react";
+import { v4 as uuidv4 } from "uuid";
 
 export const TodoContextProvider = createContext({
   todo: [],
+  newTodo: () => {},
+  handleDelete: () => {},
 });
+
+const initalState = [
+  { task: "buy food", date: " 10 / 10 / 2003", id: uuidv4() },
+];
+
+const reducer = (state, action) => {
+  let newTodoItem = state;
+  if (action.type === "New_ITEM") {
+    newTodoItem = [
+      ...state,
+      {
+        task: action.payload.newTask,
+        date: action.payload.NewDate,
+        id: uuidv4(),
+      },
+    ];
+  } else if (action.type === "ITEM_DELETE") {
+    newTodoItem = state.filter((todo) => todo.id !== action.payload.id);
+  }
+  return newTodoItem;
+};
+
+export default function TodoContextProvide({ children }) {
+  const [todo, dispatch] = useReducer(reducer, initalState);
+
+  let newTodo = (newTask, NewDate) => {
+    const newAddAction = {
+      type: "New_ITEM",
+      payload: {
+        newTask,
+        NewDate,
+      },
+    };
+    dispatch(newAddAction);
+  };
+
+  const handleDelete = (id) => {
+    dispatch({
+      type: "ITEM_DELETE",
+      payload: {
+        id,
+      },
+    });
+  };
+
+  return (
+    <TodoContextProvider.Provider
+      value={{ work: todo, deleteTodo: handleDelete, newItem: newTodo }}
+    >
+      {children}
+    </TodoContextProvider.Provider>
+  );
+}
